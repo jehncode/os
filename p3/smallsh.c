2afc948d17682a17a3db4c351865a41a9f5a5888
jenny huang <huangjen@oregonstate.edu>
2019-11-06 21:17:18 -0800

    termination with exit bool, exit signals caught

--- p3/smallsh.c
+++ p3/smallsh.c
@@ -1,7 +1,21 @@
 /* ****************************************************************************
  * Name:    Jenny Huang
  * Date:    November 4, 2019
- * Description:   
+ * Description:  
+ * Program that supports three built-in commands: exit, cd, and status
+ * Input/output redirection is not supported for these built in commands
+ *
+ * exit: exits shell and kills any other processes or jobs that the shell has
+ * has started before termination
+ *
+ * cd: changes the working directory of the shell. changes to directory
+ * specified in the HOME environment variable when no argument is presented.
+ * also supports one argument--the path of a directory to change to,
+ * supports absolute and relative paths. when shell is terminated, the 
+ * original shell it was launched from will be in its original working directory
+ *
+ * status: prints out the exit status or terminating signal of the last 
+ * foreground process
  * ***************************************************************************/
 
 #include <stdio.h>
@@ -9,6 +23,8 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+
+#define BUFFER 512
 
 void catchSIGINT(int signo) {
   char* message = "SIGINT. Use CTRL-Z to Stop.\n";
@@ -20,16 +36,17 @@ void catchSIGTSTP(int signo) {
   write(STDOUT_FILENO, message, 28);
 }
 
-void catchTermShortcut() {
-  // catch CTRL+C
+void catchTermination() {
   struct sigaction SIGINT_action = {0};
-  SIGINT_action.sa_handler = catchSIGINT;
+  struct sigaction SIGSTOP_action = {0};
+
+  // catch CTRL+C
+  SIGINT_action.sa_handler = SIG_IGN;
   sigfillset(&SIGINT_action.sa_mask);
   SIGINT_action.sa_flags = SA_RESTART;
   sigaction(SIGINT, &SIGINT_action, NULL);
 
   // catch CTRL+Z
-  struct sigaction SIGSTOP_action = {0};
   SIGSTOP_action.sa_handler = catchSIGTSTP;
   sigfillset(&SIGSTOP_action.sa_mask);
   SIGSTOP_action.sa_flags = SA_RESTART;
@@ -37,14 +54,25 @@ void catchTermShortcut() {
 }
 
 int main() {
-  catchTermShortcut();
+  catchTermination();
+
+  int process = -1; // process
+  int fin = -1;   // file input
+  int fout = -1;  // file output
+  int stat = 0;   // background status
+  char input[BUFFER];
+  memset(input, '\0', sizeof(input));
+
+  char* args[BUFFER];
+  int isbg = 0; // boolean for is background process
 
   int numCharsEntered = -5; // How many chars we entered
   int currChar = -5; // Tracks where we are when we print out every char
   size_t bufferSize = 0; // Holds how large the allocated buffer is
   char* lineEntered = NULL; // Points to a buffer allocated by getline() that holds our entered string + \n + \0
 
-  while(1) {
+  int exit = 0;
+  while(exit < 1) {
     // Get input from the user
     while(1) {
       printf("Enter in a line of text (CTRL-C to exit):");
@@ -63,6 +91,10 @@ int main() {
     lineEntered[strcspn(lineEntered, "\n")] = '\0';
     printf("Here is the cleaned line: \"%s\"\n", lineEntered);
     // Free the memory allocated by getline() or else memory leak
+    //
+    if (strcmp(lineEntered, "exit") == 0) {
+      exit++;
+    }
     free(lineEntered);
     lineEntered = NULL;
   }
