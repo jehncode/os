c916331b1e3edd1e93cac8de9eb6d2fc1bb6376d
jenny huang <huangjen@oregonstate.edu>
2019-11-07 22:15:08 -0800

    progress, not working

--- p3/smallsh.c
+++ p3/smallsh.c
@@ -24,8 +24,24 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define BUFFER 512
+#define BUFFER 256
+#define ARGSBUFFER 2048
 
+// definitions for strings/supported commands
+#define HOME "HOME"
+#define EXIT "exit"
+#define CD "cd"
+#define STATUS "status"
+
+// function declarations
+void showStatus(int childExitInteger);
+int getNumOfArgs(char* s);
+
+// struct/enums
+enum BOOL { 
+  TRUE,
+  FALSE
+};
 
 /* ****************************************************************************
  * Description:
@@ -75,10 +91,88 @@ void showStatus(int childExitInteger) {
 }
 
 /* ****************************************************************************
+ * Description:
+ * Parses string considering ' ' and returns number of segments of input
+ * @param args
+ * @param input
+ * ***************************************************************************/
+int parseInput(char** args, char* input) {
+  int n = 0;    // number of segments in input;
+  char* token = strtok(input, " \n");
+  while(token != NULL) {
+    // duplicate argument to args list
+    args[n++] = strdup(token);    
+    // update token
+    token = strtok(NULL, " \n");
+  }
+  return n;
+}
+
+/* ****************************************************************************
+ * Description:
+ * runs shell program and executes user's commands, returns 0 if terminated
+ * using exit command
+ * @param args
+ * @param n
+ * ***************************************************************************/
+int smallsh(char** args, int n) {
+  int ret = -1;
+  // check if there are any arguments
+  if (args == NULL || n < 1) {
+    return ret;
+  }
+  
+  // variables
+  enum BOOL isbkgrd = FALSE;  // boolean for is background process
+  static int status;          // status code
+
+// execute commands
+  // check if background
+  if (strcmp(args[n - 1], "&") == 0) {
+    isbkgrd++;
+    n--;
+  }
+
+  // exit command
+  if (strcmp(input, EXIT) == 0) {
+    return ++ret;
+  }
+
+  // cd command
+  if (strcmp(args[0], CD) == 0) {
+    if (n > 2) {
+      // change to directory indicated by argument 
+      chdir(args[1]);
+    } else {
+      // change to home directory
+      chdir(getenv(HOME));
+    }
+    return ret;
+  } 
+
+  // status command
+  if (strcmp(input, STATUS) == 0) {
+    showStatus(status);
+    return ret;
+  } 
+
+  // all other commands--fork()
+
+}
+
+void freeargs(char** args, int n) {
+  int i = 0;
+  for (; i < n; i++) {
+    free(args[i]);
+    args[i] = NULL;
+  }
+}
+
+/* ****************************************************************************
  * main program
  * ***************************************************************************/
 int main() {
-  // catch signals for terminating shell
+  // catch signals 
   catchTermination();
 
   int process = -1; // process
@@ -86,65 +180,50 @@ int main() {
   int fout = -1;    // file output
   int stat = 0;     // background status
 
-  char* args[BUFFER];
-  int isbg = 0; // boolean for is background process
-
-  int chars = -5; // How many chars we entered
-  // int currChar = -5; // Tracks where we are when we print out every char
+  int chars = -5;   // How many chars we entered
 
   // buffer allocated by getline() that holds our entered string + \n + \0
   char* input = NULL; 
   size_t buffer = 0; // Holds how large the allocated buffer is
 
   int exit = 0;
-  
-  // get commands
-  while(exit < 1) {
+
+  // get command & run shell
+  while(1) {   // from lecture notes
     // Get input from the user until valid not blank
     while(1) {
       printf(": ");
 
       // Get a line from the user
       chars = getline(&input, &buffer, stdin);
-      if (chars == -1)
+      if (chars == -1 || input[0] == '#')
         clearerr(stdin);
       else
         // Exit the loop - we've got input
         break; 
     }
 
-    // printf("Allocated %zu bytes for the %d chars you entered.\n", bufferSize, numCharsEntered);
-    // printf("Here is the raw entered line: \"%s\"\n", lineEntered);
-    
     // Remove the trailing \n that getline adds
     input[strcspn(input, "\n")] = '\0';
 
-    // printf("Here is the cleaned line: \"%s\"\n", lineEntered);
-    
-    // cd command
-    if (strcmp(input, "cd") == 0) {
-      if (argct > 1) {
-        // change to directory indicated by argument 
-      } else {
-        chdir(getenv("HOME"));
-      }
-    } else
-    // status command
-    if (strcmp(input, "status") == 0) {
-      showStatus(stat);
-    } else
-    // exit command
-    if (strcmp(input, "exit") == 0) {
-      exit++;
-    }
+    // parse command
+    char** args = NULL;
+    int nArgs = parseInput(args, input);
+
+    // run shell
+    int exit = smallsh(args, nArgs);
 
     // Free the memory allocated by getline() or else memory leak
     free(input);
     input = NULL;
+    freeargs(args, nArgs);
+
+    if (exit == 0) {
+      exit(0);
+    }
   }
 
   // kill processes
-  //
   //
   return 0;
 }
